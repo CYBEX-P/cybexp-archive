c2a4650f133676fcd519b2b1eda43fe95932add2
qclass <qclass@tutanota.com>
2021-07-27 11:20:42 -0700

    remove config

--- console.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-D:
-start cmd /k "call d:\venvs\analytics\Scripts\activate"
--- start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-D:
-call d:\venvs\analytics\Scripts\activate
-start /B "" python -m idlelib.idle playpen.py
